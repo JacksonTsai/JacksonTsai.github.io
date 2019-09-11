0c49d946ef0b6d1067fe2c97952e00366101af23
jackson <yj.gooogle@gmail.com>
2019-09-11 20:15:52 +0800

    chore: update dist

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'pwa-demo';
+  constructor() {
+    window.addEventListener('appinstalled', (evt) => {
+      console.log('a2hs installed');
+    });
+  }
+
 }
