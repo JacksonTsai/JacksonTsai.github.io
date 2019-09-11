681b52e64c0dffce1eb43c19a4672ab28417dad6
jackson <yj.gooogle@gmail.com>
2019-09-11 20:24:34 +0800

    chore: update dist

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'pwa-demo';
   constructor() {
     window.addEventListener('appinstalled', (evt) => {
       console.log('a2hs installed');
     });
   }
-
+  ngOnInit(){
+    window.addEventListener('appinstalled', (evt) => {
+      console.log('a2hs installed');
+    });
+  }
 }
