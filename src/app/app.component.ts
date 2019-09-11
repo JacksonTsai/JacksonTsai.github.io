89234c826de6457067ff6b521a15adb5507a6647
jackson <yj.gooogle@gmail.com>
2019-09-11 23:57:18 +0800

    fix: modify firebase format

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { SwPush } from '@angular/service-worker';
-import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
-import { Observable } from 'rxjs';
+import {
+  AngularFirestore,
+  AngularFirestoreCollection
+} from '@angular/fire/firestore';
+import { Observable, pipe } from 'rxjs';
+import { map } from 'rxjs/operators';
 export interface Item {
   error: boolean;
   data: string;
@@ -13,20 +17,22 @@ export interface Item {
 })
 export class AppComponent implements OnInit {
   title = 'pwa-demo';
-  public tasks: AngularFirestoreCollection<Item>;
-  private ItemCollection: AngularFirestoreCollection<Item>;
-  items: Observable<Item>;
+  public tasks: AngularFirestoreCollection<Item[]>;
+  // private ItemCollection: AngularFirestoreCollection<Item>;
+  // items: Observable<Item[]>;
   readonly VAPID_PUBLIC_KEY =
     'BA7qHqj9hLbQ5XqZJI6xhio7XRe9jfP4E3Btlwj37LLtTyiHOhtefTElwy8z5AZkEYNnJjYsJjjnmYH8PRlwoxs';
 
   constructor(private swPush: SwPush, private db: AngularFirestore) {
-    this.tasks = db.collection<Item>('moxa');
+    this.tasks = db.collection<Item[]>('moxa');
   }
   ngOnInit() {
     console.log('welcome Oninit!');
-    this.tasks.snapshotChanges().subscribe(value => {
-      console.log(value);
-    });
+    this.tasks
+      .snapshotChanges()
+      .pipe(map(data => data.map(v => v.payload.doc.data())))
+      .subscribe(value => {
+        console.log(...value);
+      });
   }
-
 }
